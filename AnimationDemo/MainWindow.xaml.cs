ebd1d03fa0e86fde43e271d523b28891ff65a321
PoHan Lin <pohan_lin@brightestTECH.com>
2023-08-17 15:53:48 +0800

    Add project files.

---
+++ AnimationDemo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace AnimationDemo
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Duration _openCloseDuration = new Duration(TimeSpan.FromSeconds(0.5));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dropdownContent.Height = 0;
+        }
+
+        private void OpenDropdown(object sender, RoutedEventArgs e)
+        {
+            dropdownInnerContent.Measure(new Size(dropdownContent.MaxHeight, dropdownContent.MaxWidth));
+            DoubleAnimation heightAnimation = new DoubleAnimation(dropdownInnerContent.DesiredSize.Height, _openCloseDuration)
+            {
+                AccelerationRatio = 0.75
+            };
+            dropdownContent.BeginAnimation(HeightProperty, heightAnimation);
+        }
+
+        private void CloseDropdown(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation heightAnimation = new DoubleAnimation(0, _openCloseDuration)
+            {
+                AccelerationRatio = 0.75
+            };
+            dropdownContent.BeginAnimation(HeightProperty, heightAnimation);
+        }
+    }
+}
---
+++ WPF_Animation/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace WPF_Animation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var button = sender as Button;
+            var widthAnimation = new DoubleAnimation() { To = 130, Duration = TimeSpan.FromSeconds(0.3) };
+            var heightAnimation = new DoubleAnimation() { To = 150, Duration = TimeSpan.FromSeconds(0.3) };
+
+            button.BeginAnimation(WidthProperty, widthAnimation);
+            button.BeginAnimation(HeightProperty, heightAnimation);
+
+        }
+
+        private void Button_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var button = sender as Button;
+            var widthAnimation = new DoubleAnimation() { To = 100, Duration = TimeSpan.FromSeconds(0.3) };
+            var heightAnimation = new DoubleAnimation() { To = 120, Duration = TimeSpan.FromSeconds(0.3) };
+
+            button.BeginAnimation(WidthProperty, widthAnimation);
+            button.BeginAnimation(HeightProperty, heightAnimation);
+        }
+    }
+}
